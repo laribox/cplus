1f0a3cce26ae860968647c599294078f7483bad8
ismailleet2005 <contact.hisokawebsite@gmail.com>
2015-01-21 12:47:17 +0100

    Gestion Modules Update

--- CompetencePlusForm/PackagePrecision/FormeGestionPrecision.cs
+++ CompetencePlusForm/PackagePrecision/FormeGestionPrecision.cs
@@ -26,7 +26,15 @@ namespace CompetencePlus.PackageModules
         {
 
             precisionBindingSource.DataSource = null;
-           precisionBindingSource.DataSource = new PrecisionBAO().GetallPres(m.ID);
+            precisionBindingSource.DataSource = new PrecisionBAO().GetallPres(m.ID);
+
+            Precision pr = (Precision)precisionBindingSource.Current;
+            if (pr != null)
+            {
+                dureeLabel1.Text = pr.Duree.ToString();
+                nomLabel1.Text = pr.Nom;
+               // idLabel1.Text = pr.Id.ToString();
+            }
         }
 
         private void FormeGestionPrecision_Load(object sender, EventArgs e)
@@ -36,7 +44,39 @@ namespace CompetencePlus.PackageModules
 
         private void button1_Click(object sender, EventArgs e)
         {
+            FormPrecision pr = new FormPrecision();
+            pr.getid(m);
+            pr.ShowDialog();
             this.Actualiser();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Actualiser();
+        }
+
+        private void precisionDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            Precision pr = (Precision)precisionBindingSource.Current;
+            if (e.ColumnIndex == 6)
+            {
+               
+                    new PrecisionBAO().Delete(pr.Id);
+                    this.Actualiser();
+                
+            }
+            if (e.ColumnIndex == 5)
+            {
+                FormPrecision prx = new FormPrecision();
+                prx.ismodify = true;
+                prx.getid(m);
+                prx.update(pr);
+                prx.ShowDialog();
+                this.Actualiser();
+            }
+
+
         }
     }
 }
